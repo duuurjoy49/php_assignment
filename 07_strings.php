a466581533a967ec72e5f6f28064ab8803e05481
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-08 01:00:02 +0600

    07_strings.php added

---
+++ 07_strings.php
@@ -0,0 +1,21 @@
+<?php 
+//strlen()
+echo strlen("Hello World!"); // output 12
+echo '<br>';
+
+// str_word_count()
+echo str_word_count("Hello world!"); // outputs 2
+echo '<br>';
+
+// strrev()
+echo strrev("Hello world!"); // outputs !dlrow olleH
+echo '<br>';
+
+// strpos()
+echo strpos("Hello world!", "world"); // outputs 6
+echo '<br>';
+
+//str_replace()
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+
+?>
