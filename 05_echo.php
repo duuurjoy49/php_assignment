4be0602d4e7b6f7cd25075f78d5dd6dd048ab282
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-08 00:34:03 +0600

    05_echo.php added

---
+++ 05_echo.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h2>PHP is Fun!</h2>"; // PHP is Fun!
+echo "Hello world!<br>"; // Hello world!
+echo "I'm about to learn PHP!<br>"; // I'm about to learn PHP!
+echo "This ", "string ", "was ", "made ", "with multiple parameters."; // This string was made with multiple parameters.
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>"; // Learn PHP
+echo "Study PHP at " . $txt2 . "<br>"; // Study PHP at W3Schools.com
+echo $x + $y; // 9
+
+?>
