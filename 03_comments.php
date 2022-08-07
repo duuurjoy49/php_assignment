b2fd26f06dc75cec9a9968520fcdb0711460d120
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-07 23:59:26 +0600

    03_comments.php added

---
+++ 03_comments.php
@@ -0,0 +1,16 @@
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment 
+I am Durjoy Sarker Duranta.   
+I'm from Netrakona.
+*/
+
+// commenting a part of a code line
+$x = 10 /* +20 */ +10;
+echo $x; // output => 10+10=20
+
+?>
