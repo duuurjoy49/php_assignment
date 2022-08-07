e376ce90cce755f975460d77a6cb1626d51b21c2
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-07 23:51:54 +0600

    02_syntax.php added

---
+++ 02_syntax.php
@@ -0,0 +1,20 @@
+<?php 
+    // basic syntax
+
+
+// case sensivity
+
+ECHO "Hello World!<br>";
+echo "Hello World!<br>";
+EcHo "Hello World!<br>";
+// this 3 lines will generate the same output
+
+//but variables are case sensitive
+$color = "red";
+echo "My car is " . $color . "<br>"; // My car is red
+echo "My house is " . $COLOR . "<br>"; // My house is 'error'
+echo "My boat is " . $coLOR . "<br>"; // My boat is 'error'
+
+
+
+?>
