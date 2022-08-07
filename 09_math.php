008da9bc0e1b4e55d29adcb1234a18922550821a
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-08 02:31:19 +0600

    09_math.php added

---
+++ 09_math.php
@@ -0,0 +1,23 @@
+<?php
+// pi()
+echo(pi()); //  3.1415926535898
+
+// max() and min()
+echo(min(0, 150, 30, 20, -8, -200));  //  -200
+echo(max(0, 150, 30, 20, -8, -200));  //  150
+
+// abs()
+echo(abs(-6.7));  //  6.7
+
+// sqrt ()
+echo(sqrt(64));  //  8
+
+// round()
+echo(round(0.60));  //  1
+echo(round(0.49));  //  0
+
+//  rand()
+echo(rand()); // random number
+echo(rand(10, 100)); // random number between 1-100 (inclusive)
+
+?>
