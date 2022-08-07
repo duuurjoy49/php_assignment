ec297786937e0d02c4a0fa30fcd2af3f0e11ab10
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-08 00:05:24 +0600

    04_variables.php added

---
+++ 04_variables.php
@@ -0,0 +1,14 @@
+<?php
+$txt = "Hello world!";
+$x = 5;
+$y = 10.5;
+
+$txt2 = "W3Schools.com";
+echo "I love $txt2!" . '<br>'; // output- I live W3Schools.com!
+
+// sum of 2 variables
+$x = 5;
+$y = 4;
+echo $x + $y; // 9
+
+?>
