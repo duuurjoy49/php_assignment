944868335ccf9998df43624e97bca00a04d01609
duuurjoy49 <durjoysarkerduranta@gmail.com>
2022-08-08 20:08:53 +0600

    16_01_indexed arrays.php added

---
+++ 16_01_indexed_arrays.php
@@ -0,0 +1,13 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+$arrlength = count($cars);
+
+for($x = 0; $x < $arrlength; $x++) {
+  echo $cars[$x];
+  echo "<br>";
+}
+/* output:
+Volvo
+BMW
+Toyota */
+?>
